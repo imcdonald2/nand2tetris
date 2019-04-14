9410a99225f8855a84917aeb0f104926cb42cff7
Ian McDonald <ianmc87@tuta.io>
2019-04-13 23:33:50 -0400

    mult.asm completed, test passed

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,30 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+  // Put your code here.
+  @2    //Set R2 to 0
+  M=0
+  @0
+  D=M
+  @END
+  D;JEQ  //If R0 is 0, jump to end
+
+  @1
+  D=M
+  @END
+  D;JEQ  // If R1 is 0, jump to end
+
+(LOOP)
+  @0
+  D=M
+  @2
+  M=D+M
+  @1
+  M=M-1
+  D=M
+  @LOOP
+  D;JGT  // Jump to loop when R1 is greater than 0
+
+(END)
+  @END
+  0;JMP
